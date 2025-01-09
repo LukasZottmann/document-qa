2062f7ceae5dfcfd8314a1a6edea7bab04fd6725
LukasZottmann <lukas.zottmann@institutopublix.com.br>
2025-01-09 12:52:49 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ else:
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "pdf" "md")
+        "Upload a document (.txt or .md)", type=("txt", "pdf", "md")
     )
 
     # Ask the user for a question via `st.text_area`.
